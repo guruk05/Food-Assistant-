113474a27470442923628c8150a568a64031df4c
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-23 12:24:34 +0530

    Initialising environment variables

--- Service.js
+++ Service.js
@@ -1,4 +1,9 @@
 const watson = require('watson-developer-cloud');
 
-
+const assistant = new watson.AssistantV1({
+    username: process.env.WATSON_USERNAME,
+    password: process.env.WATSON_PASSWORD,
+    url:      process.env.WATSON_URL,
+    version:  process.env.WATSON_VERSION
+});
 
