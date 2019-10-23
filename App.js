8e796aa08c84f8f2b2a4be3fbf13f059d1b54bd6
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-23 10:18:32 +0530

    imorting body parser and and dotenv

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-const express = require('express');
 require ('dotenv').config();
+const express = require('express');
 
 
-const bodyParser = require('body-parser');
-    DFAULT_BODY_SIZE = 1024*1024 * 10;
-    Default paramter limit = 10000;
+
+
+
 
