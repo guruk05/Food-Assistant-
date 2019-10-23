9aaaa37aff3475a6faf4833b87d10878a0e797b4
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-23 10:20:10 +0530

    Initializing parameter limit

--- App.js
+++ App.js
@@ -6,7 +6,10 @@ const  bodyParser = require('body-parser'),
   DEFAULT_BODY_SIZE_LIMIT = 1024 * 1024 * 10,
   DEFAULT_PARAMETER_LIMIT = 10000;
 
-
+const bodyParserJsonConfig = () => ({
+   parameterLimit: DEFAULT_PARAMETER_LIMIT,
+   limit: DEFAULT_BODY_SIZE_LIMIT
+});
 
 
 
