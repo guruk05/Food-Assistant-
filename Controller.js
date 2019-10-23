18467db77f0ec047153bdfb66272ad88f50fbdb7
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-23 12:04:11 +0530

    Initialising eports

--- Controller.js
+++ Controller.js
@@ -1,2 +1,10 @@
 const getMessage = require('./Service').getMessage;
 
+exports.ask = (req, res, next) => {
+    return getMessage(req.body)
+    .then(output => {
+        res.status(200);
+        res.send(output);
+    })
+    .catch(next);
+};
